d9e6e8fb7fa1b7c6451474922879c67e89d080b9
Sean Brennan <whalesonstilts2012@gmail.com>
2013-07-23 20:04:43 -0400

    pivoting away from musicbrainz to last fm. saving progress.

--- musicbrainz.js
+++ musicbrainz.js
@@ -9,7 +9,8 @@ var config = require('./config'),
     querystring = require('querystring'),
     xml2js = require('xml2js'),
     nano = require('nano'),
-    http = require('http');
+    http = require('http'),
+    vargs = require('vargs').Constructor;
 
 var musicbrainz_api = {
     hostname: "http://www.musicbrainz.org/",
@@ -17,22 +18,60 @@ var musicbrainz_api = {
     base_path: "/ws/2",
     headers: { "user-agent": config.user_agent },
 
-    lookup: function(entity, mbid, options) {
-        return _.extend(this, {
-            path: this.base_path + "/" + entity + "/" + mbid + "?" + querystring.stringify(options)
+    lookup: function(/* entity, mbid, [options,] callback */) {
+        var args = new (vargs)(arguments);
+        if(!args.callbackGiven()) {
+            console.log("[ERROR] musicbrainz API functions need a valid callback function.")
+            return null;
+        }
+
+        var entity = args.first,
+            mbid = args.at(1),
+            options = args.last,
+            callback = args.callback,
+            hasOptions = args.length > 2;
+        var path = this.base_path + "/" + entity + "/" + mbid + (hasOptions ? "?" + querystring.stringify(options) : "");
+        return http.get(_.extend(this, { path: path }), function(res) {
+            res.setEncoding("utf8");
+            res.on("data", callback);
         });
     },
 
-    browse: function(entity, options) {
-        return _.extend(this, {
-            path: this.base_path + "/" + entity + "?" + querystring.stringify(options)
+    browse: function(/* entity, [options,] callback */) {
+        var args = new (vargs)(arguments);
+        if(!args.callbackGiven()) {
+            console.log("[ERROR] musicbrainz API functions need a valid callback function.")
+            return null;
+        }
+        
+        var entity = args.first,
+            options = args.last,
+            callback = args.callback,
+            hasOptions = args.length > 1;
+        var path = this.base_path + "/" + entity + (hasOptions ? "?" + querystring.stringify(options) : "");
+        return http.get(_.extend(this, { path: path }), function(res) {
+            res.setEncoding("utf8");
+            res.on("data", callback);
         });
     },
 
-    search: function(entity, qs, options) {
-        var qs_opts = _.extend(options, { query: qs });
-        return _.extend(this, {
-            path: this.base_path + "/" + entity + "?" + querystring.stringify(qs_opts)
+    search: function(/* entity, querystring, [options,] callback */) {
+        var args = new (vargs)(arguments);
+        if(!args.callbackGiven()) {
+            console.log("[ERROR] musicbrainz API functions need a valid callback function.")
+            return null;
+        }
+        
+        var entity = args.first,
+            qs = args.at(1),
+            options = args.last,
+            callback = args.callback,
+            hasOptions = args.length > 2;
+        var qs_opts = hasOptions ? _.extend(options, { query: qs }) : { query: qs };
+        var path = this.base_path + "/" + entity + "?" + querystring.stringify(qs_opts);
+        return http.get(_.extend(this, { path: path }), function(res) {
+            res.setEncoding("utf8");
+            res.on("data", callback);
         });
     }
 };
@@ -49,43 +88,68 @@ var musicbrainz_api = {
     var couch = nano(config.database.host + ":" + config.database.port),
         db = couch.use(config.database.name);
     // TODO: initialize database if it doesn't exist
-    var lookup_queue = [],
-        found_artists = [];
+    /*var lookup_queue = [],
+        found_artists = [];*/
+    var artist_ids = [];
 
     // TODO: loop queries, space each one by 1 sec
     // initial query for artists
 
-    // artist + tag search query
-    var artist_count = 0,
-        artist_page = 0;
-    var artist_search = function() {
-        http.get(musicbrainz_api.search("artist", "tag:(hip hop OR rap)", { limit: 100, offset: artist_page }), function(res) {
-            res.setEncoding("utf8");
-            res.on("data", function(chunk) {
-                xml2js.parseString(chunk, function(err, body) {
-                    if(err)
-                        console.log("[ERROR] " + err);
-                    var artists = body.metadata["artist-list"][0].artist;
-                    artist_count = body.metadata["artist-list"][0].$.count;
-                    artist_page += artists.length;
-                    _.each(artists, function(artist) {
-                        if(!_.contains(found_artists, artist.$.id))
-                            lookup_queue.push({ type: "artist", data: artist });
-                    });
-                    // keep searching, or move on to lookups
-                    if(artist_page < artist_count)
-                        setTimeout(artist_search, 1000);
-                    else
-                        setTimeout(artist_lookup, 1000);
+    var request_queue = function(/* artist_callback, release_callback  */) {
+        var args = new (vargs)(arguments);
+        if(args.length <= 0 || !args.callbackGiven())
+            console.log("[ERROR] request_queue requires two callback functions.");
+        else {
+            this.artist_callback = args.first;
+            this.release_callback = args.callback;
+            this.requests = [];
+            this.tick = 1000; // # millis to wait between reqs
+            this.page = 0;
+            this.count = 0;
+
+            this.next = this.run = function() {
+                var req = this.requests[0];
+                this.requests = _.rest(this.requests);
+
+                if(req.type == "artist search") {
+
+                } else if(req.type == "release browse") {
+
+                }
+            };
+        }
+    };
+    var artist_search = function(/* ... */) {
+        musicbrainz_api.search("artist", "tag:(hip hop OR rap)", { limit: 100, offset: artist_page }, function(chunk) {
+            xml2js.parseString(chunk, function(err, body) {
+                if(err)
+                    console.log("[ERROR] " + err);
+                var artists = body.metadata["artist-list"][0].artist;
+                artist_count = body.metadata["artist-list"][0].$.count;
+                artist_page += artists.length;
+                _.each(artists, function(artist) {
+                    if(!_.contains(found_artists, artist.$.id)) {
+                        lookup_queue.push({ type: "artist", data: artist });
+                        found_artists = _.union(found_artists, [artist.$.id]);
+                    }
                 });
+                // keep searching, or move on to lookups
+                if(artist_page < artist_count)
+                    setTimeout(artist_search, 1000);
+                else
+                    setTimeout(artist_lookup, 1000);
             });
         }).on("error", function(e) {
             console.log("[ERROR] " + e.message);
             setTimeout(artist_search, 1000);
         });
     };
-    var artist_lookup = function() {
-        // ...
+    var artist_lookup = function(/* ... */) {
+        var artist = lookup_queue[0];
+        lookup_queue = _.rest(lookup_queue);
+        musicbrainz_api.browse("recording", { limit: 100 }, function(chunk) {
+
+        });
     };
 
     artist_search();
